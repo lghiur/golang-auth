2bb429226ca521f56fefa8280bb3f62f93603f25
Laurentiu Ghiur <laurentiu.ghiur@gmail.com>
2020-09-08 10:42:59 +0300

    unmarshal json

--- main.go
+++ main.go
@@ -27,5 +27,15 @@ func main() {
 		log.Panic(err)
 	}
 
-	fmt.Println(string(bs))
+	fmt.Println("Print JSON", string(bs))
+
+	xp2 := []person{}
+
+	err = json.Unmarshal(bs, &xp2)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	fmt.Println("back in GO data structire", xp2)
 }
