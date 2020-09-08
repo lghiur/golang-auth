7d0ddbd0bcbe5a133618e4679bbdf58f054b9d2e
Laurentiu Ghiur <laurentiu.ghiur@gmail.com>
2020-09-08 10:59:31 +0300

    init server, encode and decode json

--- main.go
+++ main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
+	"net/http"
 )
 
 type person struct {
@@ -11,31 +11,59 @@ type person struct {
 }
 
 func main() {
+	// p1 := person{
+	// 	First: "Jenny",
+	// }
+
+	// p2 := person{
+	// 	First: "James",
+	// }
+
+	// xp := []person{p1, p2}
+
+	// bs, err := json.Marshal(xp)
+
+	// if err != nil {
+	// 	log.Panic(err)
+	// }
+
+	// fmt.Println("Print JSON", string(bs))
+
+	// xp2 := []person{}
+
+	// err = json.Unmarshal(bs, &xp2)
+
+	// if err != nil {
+	// 	log.Panic(err)
+	// }
+
+	// fmt.Println("back in GO data structire", xp2)
+
+	http.HandleFunc("/encode", foo)
+	http.HandleFunc("/decode", bar)
+	http.ListenAndServe(":1111", nil)
+}
+
+func foo(w http.ResponseWriter, r *http.Request) {
 	p1 := person{
 		First: "Jenny",
 	}
 
-	p2 := person{
-		First: "James",
-	}
-
-	xp := []person{p1, p2}
-
-	bs, err := json.Marshal(xp)
+	err := json.NewEncoder(w).Encode(p1)
 
 	if err != nil {
-		log.Panic(err)
+		log.Println("Encode bad data", err)
 	}
+}
 
-	fmt.Println("Print JSON", string(bs))
+func bar(w http.ResponseWriter, r *http.Request) {
+	var p1 person
 
-	xp2 := []person{}
-
-	err = json.Unmarshal(bs, &xp2)
+	err := json.NewDecoder(r.Body).Decode(&p1)
 
 	if err != nil {
-		log.Panic(err)
+		log.Println("Decode bad data", err)
 	}
 
-	fmt.Println("back in GO data structire", xp2)
+	log.Println("Go data after decode", p1)
 }
