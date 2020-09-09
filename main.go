e9b7190267e254fbf1d95beb0bf842565c20db73
Laurentiu Ghiur <laurentiu.ghiur@gmail.com>
2020-09-09 11:05:03 +0300

    base64 user pass example

--- main.go
+++ main.go
@@ -1,69 +1,10 @@
 package main
 
 import (
-	"encoding/json"
-	"log"
-	"net/http"
+	"encoding/base64"
+	"fmt"
 )
 
-type person struct {
-	First string
-}
-
 func main() {
-	// p1 := person{
-	// 	First: "Jenny",
-	// }
-
-	// p2 := person{
-	// 	First: "James",
-	// }
-
-	// xp := []person{p1, p2}
-
-	// bs, err := json.Marshal(xp)
-
-	// if err != nil {
-	// 	log.Panic(err)
-	// }
-
-	// fmt.Println("Print JSON", string(bs))
-
-	// xp2 := []person{}
-
-	// err = json.Unmarshal(bs, &xp2)
-
-	// if err != nil {
-	// 	log.Panic(err)
-	// }
-
-	// fmt.Println("back in GO data structire", xp2)
-
-	http.HandleFunc("/encode", foo)
-	http.HandleFunc("/decode", bar)
-	http.ListenAndServe(":1111", nil)
-}
-
-func foo(w http.ResponseWriter, r *http.Request) {
-	p1 := person{
-		First: "Jenny",
-	}
-
-	err := json.NewEncoder(w).Encode(p1)
-
-	if err != nil {
-		log.Println("Encode bad data", err)
-	}
-}
-
-func bar(w http.ResponseWriter, r *http.Request) {
-	var p1 person
-
-	err := json.NewDecoder(r.Body).Decode(&p1)
-
-	if err != nil {
-		log.Println("Decode bad data", err)
-	}
-
-	log.Println("Go data after decode", p1)
+	fmt.Println(base64.StdEncoding.EncodeToString([]byte("user:pass")))
 }
